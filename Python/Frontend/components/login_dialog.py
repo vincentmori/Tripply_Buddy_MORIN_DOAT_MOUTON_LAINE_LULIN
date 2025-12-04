17fc6de57deafea17130d86b5b5c0fe662524961
vincentmori <vincent.morin@edu.ece.fr>
2025-12-04 19:34:33 +0100

    Modif couleur pr que streamlit gere auto + deplacer bouton connecter en face register sur loginn dialog

--- Python/Frontend/components/login_dialog.py
+++ Python/Frontend/components/login_dialog.py
@@ -15,42 +15,42 @@ def login_dialog():
     user_id = st.text_input("Id")
     password = st.text_input("Password", type='password')
     
-    col1, _, col2 = st.columns([2, 3, 2])
-    with col2:
-        remember_me = False
+    remember_me = False
+    col1, col2 = st.columns([4, 2])
 
     with col1:
-        if st.button("Register", key="to_register_btn"):
+        if st.button("No account? Register", key="to_register_btn"):
             st.session_state.app_mode = 'register' # Changement d'état pour creer un compte
             st.rerun()
 
-    if st.button("Connect", use_container_width=True):
-        if not user_id.strip() or not password.strip():
-            st.error("Please enter your ID and Password.")
-        else:
-            check_co, message_erreur = check_connexion(user_id, password) # Check validite de la connexion 
-            
-            if not check_co:
-                st.error(message_erreur)
-            else:                
-                if remember_me:
-                    try:
-                        with open(SESSION_FILE, "w") as f:
-                            # Ecrit dans un txt les user_id et mot de pasee utilisatuer si l'utilisateur veut rester connecter
-                            f.write(f"{user_id}|{password}") 
-                    except Exception as e:
-                        print(f"Impossible to write: {e}")
-                else:
-                    if os.path.exists(SESSION_FILE):
-                        os.remove(SESSION_FILE)
+    with col2:
+        if st.button("Connect", use_container_width=True):
+            if not user_id.strip() or not password.strip():
+                st.error("Please enter your ID and Password.")
+            else:
+                check_co, message_erreur = check_connexion(user_id, password) # Check validite de la connexion 
                 
-                sleep(0.5)
-                
-                with st.spinner("⏳ Loading data and computing your recommandations..."):
-                    # Initialise l'utilisateur 
-                    init_user(user_id) 
-                
-                st.success("🎉 Connection Successful! Your recommandations are ready!")
-                
-                
-                st.rerun()
+                if not check_co:
+                    st.error(message_erreur)
+                else:                
+                    if remember_me:
+                        try:
+                            with open(SESSION_FILE, "w") as f:
+                                # Ecrit dans un txt les user_id et mot de pasee utilisatuer si l'utilisateur veut rester connecter
+                                f.write(f"{user_id}|{password}") 
+                        except Exception as e:
+                            print(f"Impossible to write: {e}")
+                    else:
+                        if os.path.exists(SESSION_FILE):
+                            os.remove(SESSION_FILE)
+                    
+                    sleep(0.5)
+                    
+                    with st.spinner("⏳ Loading data and computing your recommandations..."):
+                        # Initialise l'utilisateur 
+                        init_user(user_id) 
+                    
+                    st.success("🎉 Connection Successful! Your recommandations are ready!")
+                    
+                    
+                    st.rerun()
